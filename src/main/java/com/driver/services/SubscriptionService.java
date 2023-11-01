2395ecea01a737b5d849ad199380e135c4be21ea
Namdev Arondekar <arvandekarnamdev22@gmail.com>
2023-11-02 05:24:09 +0530

    try3

--- src/main/java/com/driver/services/SubscriptionService.java
+++ src/main/java/com/driver/services/SubscriptionService.java
@@ -72,8 +72,7 @@ public class SubscriptionService {
             subscriptionRepository.save(subscription);
             return  New-old;
         }
-        return -1;
-
+        throw new Exception("Already the best Subscription");
         //If you are already at an ElITE subscription : then throw Exception ("Already the best Subscription")
         //In all other cases just try to upgrade the subscription and tell the difference of price that user has to pay
         //update the subscription in the repository
